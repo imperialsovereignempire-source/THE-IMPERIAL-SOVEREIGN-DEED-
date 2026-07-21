1abc545fefb7cac923473a7cb51a6de22ba33f35
NOOM <imperialsovereignempire@gmail.com>
2026-07-21 20:07:09 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        void: '#0a1128',
+        blueCore: '#2563eb',
+        blueLight: '#60a5fa',
+        royal: '#a855f7'
+      },
+      fontFamily: {
+        cinzel: ['Cinzel', 'serif'],
+        mono: ['JetBrains Mono', 'monospace']
+      }
+    }
+  },
+  plugins: [],
+}
